6419add9dd6d7694b4b5c44439a3bc2cbb47b0a6
mahdi zarepour <mahdizarepour15@gmail.com>
2021-11-16 10:44:26 +0330

    Add Features To Admin Panel(Shop).

--- shop/admin.py
+++ shop/admin.py
@@ -3,5 +3,34 @@ from .models import Category, Product
 
 
 
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'name', 'price', 'available')
+    list_filter = ('available',)
+    search_fields = ('name', 'description')
+    list_editable = ('price', 'available')
+    prepopulated_fields = {'slug': ('name',)}
+    raw_id_fields = ('category',)
+    actions = ('make_available', 'make_unavailable')
+
+
+    def make_available(self, request, queryset):
+        # queryset: selected object and return count of selected
+        rows = queryset.update(available=True)
+        self.message_user(request, f'{rows} items change to available')
+    make_available.short_description = 'make all available'
+
+    def make_unavailable(self, request, queryset):
+        # queryset: selected object and return count of selected
+        rows = queryset.update(available=False)
+        self.message_user(request, f'{rows} items change to unavailable')
+    make_unavailable.short_description = 'make all unavailable'
+    
